ed6a989fca7066cd37ff881cdec2a8f6b4cd4aa5
morganwilde <wilde.morgan@gmail.com>
2013-03-28 16:59:24 +0200

    del

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 require_once 'classes/environment.class.php';
 require_once 'classes/news.class.php';
 require_once 'classes/actions.class.php';
